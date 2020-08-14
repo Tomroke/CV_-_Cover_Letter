263d75b860d7d9f7bdea30c0e4e1c5e6d30de949
J. Tommy Irving <j.t.i@hotmail.se>
2020-08-14 14:44:17 +0200

    Added uniform icons for the ContactMeFragment.kt and added Glide to the app. Also made a BindingAdapter for the images that will be displayed in the CVFragment.kt.

--- app/src/main/java/com/irving/cvpersonalletter/BindingUtils.kt
+++ app/src/main/java/com/irving/cvpersonalletter/BindingUtils.kt
@@ -1,21 +1,39 @@
+/*
+* The icons used are from the site www.flaticon.com.
+* The linkedin & github icon were made by Freepik found at https://www.flaticon.com/authors/freepik,
+* wile the email & phone icon were made by Gregor Cresnar found at https://www.flaticon.com/authors/gregor-cresnar
+* */
+
 package com.irving.cvpersonalletter
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.irving.cvpersonalletter.database.ContactMeData
 
-@BindingAdapter("contactingIcon")
+@BindingAdapter("setContactingIcon")
 fun ImageView.setContactingIcon (item: ContactMeData?){
     item?.let {
         setImageResource(
             when (item.contactingIcon){
                 1 -> R.drawable.ic_linkedin
                 2 -> R.drawable.ic_email
-                3 -> R.drawable.ic_old_phone
+                3 -> R.drawable.ic_phone
                 4 -> R.drawable.ic_github
                 else -> R.drawable.ic_linkedin
 
             }
         )
+    }
+}
+
+@BindingAdapter ("setCvImages")
+fun setCvImages (view: ImageView, url: String?){
+    if (!url.isNullOrEmpty()){
+        Glide.with(view.context)
+            .load(url)
+            .placeholder(R.drawable.ic_phone)
+            .circleCrop()
+            .into(view)
     }
 }
